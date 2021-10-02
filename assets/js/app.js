3db8aecdb7e05ec0c0582758604f3d93b21e89db
CCARTES4 <ccartes4@gmail.com>
2021-10-02 18:19:18 -0300

    commands per function added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,179 +1,205 @@
-const productList = document.querySelector('#product-grid');
-const nav = document.querySelector('#ul');
-let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
-let categorySelect = document.querySelector('#category');
-const resetSearch = document.querySelector('#resetSearch');
-const searchInput = document.querySelector('#productSearch');
+document.addEventListener('DOMContentLoaded',() =>{
+
+    const productList = document.querySelector('#product-grid');
+    const nav = document.querySelector('#ul');
+    let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
+    let categorySelect = document.querySelector('#category');
+    const resetSearch = document.querySelector('#resetSearch');
+    const searchInput = document.querySelector('#productSearch');
 
 
-loadEventListeners()
+    loadEventListeners()
 
-/**
- * Esta función se encarga de cargar todos los metodos necesarios para el funcionamiento inicial de la aplicación, ya sea renderizado
- * de todos los productos, cambio según opcion seleccionada en el SELECT 
- */
-function loadEventListeners() {
-    renderCartQuantity()
-    loadProducts();
-    productList.addEventListener('click', addToCart)
-    categorySelect.addEventListener('change', (e) => {
-        let categoryId = categorySelect.options[categorySelect.selectedIndex].value;
-        loadProducts(categoryId);
-    });
-    resetSearch.addEventListener('click', restoreSearch);
-    searchInput.addEventListener('change',searchProduct);
-}
-
-/**
- * Está función se encarga de realizar la consulta a la API para retornar todos los productos disponibles. Como la API puede recibir un parámetro opcional, 
- * se realiza una validación en la cuál si se envía el parametro opcional, la url variará y de lo contrario, esta quedará como estaba definida en un principio.
- * @param {int} categoryId 
- */
-function loadProducts(categoryId) {
-
-    try {
-        let url = `https://ccartes.000webhostapp.com/products.php`;
-
-        categoryId !== undefined ? url = `https://ccartes.000webhostapp.com/products.php?categoria=${categoryId}` : url;
-
-        fetch(url, {
-            method: 'GET'
-        })
-        .then(result => result.json())
-        .then(data => renderProducts(data));
-        cleanHTML();
-    } catch (error) {
-        alert(`Oh no! Ha habido un problema al cargar los productos ${error}`)
-    }
-    
-}
-
-/**
- * Esta función se encarga de todo el renderizado de los productos recibidos en el llamado a la API
- * @param {array} products 
- */
-function renderProducts(products) {
-    products.forEach(product => {
-
-        const { id, name, url_image, price, discount, category } = product;
-        const noImage = './assets/img/noImage.jpg'
-        productList.innerHTML += `
-            <div class="col-12 col-md-4 col-sm-4 col-lg-3 mt-4">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-img-actions">
-                            <img id="productImage" src="${url_image ? url_image : noImage}" class="card-img img-fluid" alt="img_product">
-                        </div>
-                    </div>
-                    <div class="card-body bg-light text-center">
-                        <div class="mb-2">
-                            <h6 class="font-weight-semibold mb-2">
-                                <input id="cod" hidden="true" value="${id}"> </input> 
-                                <input id="discount" hidden="true" value="${discount}"> </input> 
-                                <input id="category" hidden="true" value="${category}"> </input>
-                                <a id="name" href="#" class="text-default mb-2" data-abc="true">${name}</a>
-                            </h6>
-                        </div>
-                        <h3 id="price" class="mb-0 font-weight-semibold">$${price}</h3>
-                        <button id="bg-carrito" type="button" class="btn bg-cart">Añadir al carrito</button>
-                    </div>
-                </div>
-            </div>
-        `;
-    });
-}
-
-/**
- * 
- * @param {event} e 
- */
-function addToCart(e) {
-    e.preventDefault();
-
-    if (e.target.classList.contains('bg-cart')) {
-        const selectedProduct = e.target.parentElement.parentElement;
-        readProductData(selectedProduct);
-    }
-}
-
-function readProductData(product) {
-
-    const products = {
-        id: product.querySelector('#cod').value,
-        name: product.querySelector('#name').textContent,
-        url_image: product.querySelector('#productImage').src,
-        price: product.querySelector('#price').textContent,
-        discount: product.querySelector('#discount').value,
-        category: product.querySelector('#category').value,
-        quantity: 1
+    /**
+     * Esta función se encarga de cargar todos los metodos necesarios para el funcionamiento inicial de la aplicación, ya sea renderizado
+     * de todos los productos, cambio según opcion seleccionada en el SELECT 
+     */
+    function loadEventListeners() {
+        renderCartQuantity()
+        loadProducts();
+        productList.addEventListener('click', addToCart)
+        categorySelect.addEventListener('change', (e) => {
+            let categoryId = categorySelect.options[categorySelect.selectedIndex].value;
+            loadProducts(categoryId);
+        });
+        resetSearch.addEventListener('click', restoreSearch);
+        searchInput.addEventListener('change',searchProduct);
     }
 
-    const exist = shoppingCart.some(product => product.id === products.id);
+    /**
+     * Está función se encarga de realizar la consulta a la API para retornar todos los productos disponibles. Como la API puede recibir un parámetro opcional, 
+     * se realiza una validación en la cuál si se envía el parametro opcional, la url variará y de lo contrario, esta quedará como estaba definida en un principio.
+     * @param {int} categoryId 
+     */
+    function loadProducts(categoryId) {
 
-    if (exist) {
-        const updatedProduct = shoppingCart.map(product => {
-            if (product.id === products.id) {
-                product.quantity++;
-                return product;
-            } else {
-                return product;
-            }
-        })
-        shoppingCart = [...updatedProduct]
-    } else {
-        shoppingCart = [...shoppingCart, products];
+        try {
+            let url = `https://ccartes.000webhostapp.com/products.php`;
+
+            categoryId !== undefined ? url = `https://ccartes.000webhostapp.com/products.php?categoria=${categoryId}` : url;
+
+            fetch(url, {
+                method: 'GET'
+            })
+            .then(result => result.json())
+            .then(data => renderProducts(data));
+            cleanHTML();
+        } catch (error) {
+            alert(`Oh no! Ha habido un problema al cargar los productos ${error}`)
+        }
         
     }
-    renderCartQuantity();
-    setLocalstorage();
-}
 
-function renderCartQuantity(){
-    const exist = document.querySelector('#acc');
+    /**
+     * Esta función se encarga de todo el renderizado de los productos recibidos en el llamado a la API
+     * @param {array} products 
+     */
+    function renderProducts(products) {
+        products.forEach(product => {
 
-    const list = document.createElement('li');
-    const acc = document.createElement('a');
-
-    if (exist) {
-        exist.innerText = shoppingCart.length;
-    } else {
-        acc.classList.add('btn', 'btn-danger', 'carQuantity');
-        list.classList.add('nav-item');
-        acc.setAttribute('id', 'acc');
-        acc.innerText = shoppingCart.length;
-        list.appendChild(acc);
-        nav.appendChild(list);
+            const { id, name, url_image, price, discount, category } = product;
+            const noImage = './assets/img/noImage.jpg'
+            productList.innerHTML += `
+                <div class="col-12 col-md-4 col-sm-4 col-lg-3 mt-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-img-actions">
+                                <img id="productImage" src="${url_image ? url_image : noImage}" class="card-img img-fluid" alt="img_product">
+                            </div>
+                        </div>
+                        <div class="card-body bg-light text-center">
+                            <div class="mb-2">
+                                <h6 class="font-weight-semibold mb-2">
+                                    <input id="cod" hidden="true" value="${id}"> </input> 
+                                    <input id="discount" hidden="true" value="${discount}"> </input> 
+                                    <input id="category" hidden="true" value="${category}"> </input>
+                                    <a id="name" href="#" class="text-default mb-2" data-abc="true">${name}</a>
+                                </h6>
+                            </div>
+                            <h3 id="price" class="mb-0 font-weight-semibold">$${price}</h3>
+                            <button id="bg-carrito" type="button" class="btn bg-cart">Añadir al carrito</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
     }
-}
 
-function searchProduct(e){
-    let search = e.target.value;
-    try {
-        let url = `https://ccartes.000webhostapp.com/products.php?nombre=${search}`;
+    /**
+     * 
+     * @param {event} e 
+     */
+    function addToCart(e) {
+        e.preventDefault();
 
-        fetch(url, {
-            method: 'GET'
-        })
-        .then(result => result.json())
-        .then(data => renderProducts(data));
-        cleanHTML();
-    } catch (error) {
-        alert(`Oh no! Ha habido un problema al cargar los productos ${error}`)
+        if (e.target.classList.contains('bg-cart')) {
+            const selectedProduct = e.target.parentElement.parentElement;
+            readProductData(selectedProduct);
+        }
     }
-}
 
-function cleanHTML() {
-    while (productList.firstChild) {
-        productList.removeChild(productList.firstChild);
+    /**
+     * Esta función se encarga de guardar el producto seleccionado por el cliente dentro de un array y posteriormente, realizar la validación de si el producto ya existe 
+     * o no dentro del mismo, en caso de existir, actualiza la cantidad de este y de lo contrario, lo agrega al arreglo. 
+     * @param {array} product 
+     */
+    function readProductData(product) {
+
+        const products = {
+            id: product.querySelector('#cod').value,
+            name: product.querySelector('#name').textContent,
+            url_image: product.querySelector('#productImage').src,
+            price: product.querySelector('#price').textContent,
+            discount: product.querySelector('#discount').value,
+            category: product.querySelector('#category').value,
+            quantity: 1
+        }
+
+        const exist = shoppingCart.some(product => product.id === products.id);
+
+        if (exist) {
+            const updatedProduct = shoppingCart.map(product => {
+                if (product.id === products.id) {
+                    product.quantity++;
+                    return product;
+                } else {
+                    return product;
+                }
+            })
+            shoppingCart = [...updatedProduct]
+        } else {
+            shoppingCart = [...shoppingCart, products];
+            
+        }
+        renderCartQuantity();
+        setLocalstorage();
     }
-}
 
-function setLocalstorage() {
-    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-}
+    /**
+     * Esta función se encarga de ir actualizando el contador de productos. Es un indicador visual para el usuario saber cuántos productos tiene agregados en su carrito. 
+     */
+    function renderCartQuantity(){
+        const exist = document.querySelector('#acc');
 
-function restoreSearch() {
-    categorySelect.selectedIndex = 0;
-    loadProducts();
-}
+        const list = document.createElement('li');
+        const acc = document.createElement('a');
 
+        if (exist) {
+            exist.innerText = shoppingCart.length;
+        } else {
+            acc.classList.add('btn', 'btn-danger', 'carQuantity');
+            list.classList.add('nav-item');
+            acc.setAttribute('id', 'acc');
+            acc.innerText = shoppingCart.length;
+            list.appendChild(acc);
+            nav.appendChild(list);
+        }
+    }
+
+    /**
+     * Esta función se encarga de retornar los productos que contengan en su nombre los caracteres indicados por el usuario en el input de buscar.
+     * Realiza una consulta a la API y va retornado los productos según correspondan. 
+     * @param {event} e 
+     */
+    function searchProduct(e){
+        let search = e.target.value;
+        try {
+            let url = `https://ccartes.000webhostapp.com/products.php?nombre=${search}`;
+
+            fetch(url, {
+                method: 'GET'
+            })
+            .then(result => result.json())
+            .then(data => renderProducts(data));
+            cleanHTML();
+        } catch (error) {
+            alert(`Oh no! Ha habido un problema al cargar los productos ${error}`)
+        }
+    }
+
+    /**
+     * Esta función se encarga de remover los productos del HTML, en caso de que el usuario realice la acción de buscar, ya sea según categoría o nombre
+     */
+    function cleanHTML() {
+        while (productList.firstChild) {
+            productList.removeChild(productList.firstChild);
+        }
+    }
+
+    /**
+     * Esta función se encarga de almacenar los productos en el localStorage del navegador para conservarlos en caso de que se cierre la página. 
+     */
+    function setLocalstorage() {
+        localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+    }
+
+    /**
+     * Cuando el usuario realiza una búsqueda ya sea por categoría o por nombre, pero posterior a ello desea volver a ver todos los productos, esta función se encarga de
+     * retornar el listado completo.
+     */
+    function restoreSearch() {
+        categorySelect.selectedIndex = 0;
+        loadProducts();
+    }
+
+})
--- assets/js/shoppingCart.js
+++ assets/js/shoppingCart.js
@@ -1,67 +1,93 @@
-let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
-const shoppingCartList = document.querySelector('#shoppingCartList tbody');
-const shoppingTable = document.querySelector('#shoppingCartList');
-
-
-loadEventListeners();
-
-function loadEventListeners() {
-    renderShoppingCart();
-    shoppingTable.addEventListener('click', deleteProduct);
-}
-
-function restoreCart(){
-
-    while (shoppingCartList.firstChild) {
-        shoppingCartList.removeChild(shoppingCartList.firstChild);
-    }
-}
-
-function renderShoppingCart() {
-    const noImage = './assets/img/noImage.jpg'
-    restoreCart()
-
-    shoppingCart.forEach( product => {
-        const { id, name, url_image, price, discount, category, quantity } = product;
-        const row = document.createElement('tr');
-        row.innerHTML = `
-        <td> 
-            <img src="${url_image ? url_image : noImage}" width="60" height="60" >
-        </td>
-        <td> 
-            ${name}
-        </td>
-        <td> 
-            ${price}
-        </td>
-        <td> 
-            ${quantity}
-        </td>
-        <td> 
-            <a href="#" class="btn btn-danger deleteProduct" data-id=${id}> Quitar producto </a>
-        </td>
-        `;
-        shoppingCartList.appendChild(row);
-    })
-}
-
-function deleteProduct(e) {
-    if (e.target.classList.contains('deleteProduct')) {
-        const productId = (e.target.getAttribute('data-id'));
-
-        shoppingCart = shoppingCart.filter( product => product.id !== productId)
-
-        clearLocalStorage();
-        setLocalstorage();
-        renderShoppingCart()
-    }
+document.addEventListener('DOMContentLoaded',() => {
     
-}
 
-function setLocalstorage() {
-    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-}
+    /**
+     * Creación de selectores que serán utilizados posteriormente.
+     */
+    let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+    const shoppingCartList = document.querySelector('#shoppingCartList tbody');
+    const shoppingTable = document.querySelector('#shoppingCartList');
 
-function clearLocalStorage() {
-    localStorage.clear();
-}
+    /**
+     * Carga de listeners
+     */
+    loadEventListeners();
+
+    /**
+     * Creación de listeners
+     */
+    function loadEventListeners() {
+        renderShoppingCart();
+        shoppingTable.addEventListener('click', deleteProduct);
+    }
+
+    /**
+     * Esta función se encarga de limpiar el carro de compras en caso de que se hagan cambios
+     * en el array que contiene los productos agregados a este.
+     */
+    function restoreCart(){
+
+        while (shoppingCartList.firstChild) {
+            shoppingCartList.removeChild(shoppingCartList.firstChild);
+        }
+    }
+
+    /**
+     * Esta función se encarga de recorrer nuestro arreglo del carrito de compras para posteriormente, renderizarlo en forma de HTML
+     * para la visualización de estos. 
+     */
+    function renderShoppingCart() {
+        const noImage = './assets/img/noImage.jpg'
+        restoreCart()
+
+        shoppingCart.forEach( product => {
+            const { id, name, url_image, price, discount, category, quantity } = product;
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td> 
+                <img src="${url_image ? url_image : noImage}" width="60" height="60" >
+            </td>
+            <td> 
+                ${name}
+            </td>
+            <td> 
+                ${price}
+            </td>
+            <td> 
+                ${quantity}
+            </td>
+            <td> 
+                <a href="#" class="btn btn-danger deleteProduct" data-id=${id}> Quitar producto </a>
+            </td>
+            `;
+            shoppingCartList.appendChild(row);
+        })
+    }
+
+    /**
+     * Esta función se encarga de eliminar un producto de nuestro array de carrito de compras.
+     * Realiza una comprobación para detectar si el producto seleccionado se encuentra dentro de este.
+     * Una vez que lo encuentra, limpia el localStorage para posteriormente guardarlo nuevamente y volver a renderizar el nuevo carrito de compras
+     * @param {event} e 
+     */
+    function deleteProduct(e) {
+        if (e.target.classList.contains('deleteProduct')) {
+            const productId = (e.target.getAttribute('data-id'));
+
+            shoppingCart = shoppingCart.filter( product => product.id !== productId)
+
+            clearLocalStorage();
+            setLocalstorage();
+            renderShoppingCart()
+        }
+        
+    }
+
+    function setLocalstorage() {
+        localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+    }
+
+    function clearLocalStorage() {
+        localStorage.clear();
+    }
+})
